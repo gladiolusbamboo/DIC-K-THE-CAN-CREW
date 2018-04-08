e9b1511a8ed69273daa717d957285d6bf2e5511f
Yuji Koyama <gladiolus_vstl@gladiolus.egoism.jp>
2018-04-08 07:52:42 +0000

    180408 検索結果のハイライト、検索ヒット数脳表示など機能追加

--- app/helpers/search_helper.rb
+++ app/helpers/search_helper.rb
@@ -1,2 +1,12 @@
 module SearchHelper
+  def get_unique_songs_sum lyrics_array
+    arr = []
+    lyrics_array.each do |lyric|
+      if !arr.include? lyric.song.id
+        arr.push lyric.song.id
+      end
+    end
+    pp arr
+    arr.length
+  end
 end
