6c8fd789ab3fc90a22ce68bab2d8a773bb940bd7
Yuji Koyama <gladiolus_vstl@gladiolus.egoism.jp>
2018-04-11 13:16:49 +0000

    180411 表記での検索を実装

--- app/helpers/search_helper.rb
+++ app/helpers/search_helper.rb
@@ -1,51 +1,55 @@
 module SearchHelper
-  def extract_search_word word, lyric
-    reg = /(.{0,5}#{word}.{0,5})/
-    matched = reg.match(lyric)
-    if matched
-      matched[0]
-    else
-      ""
-    end
-  end
-
   def show_searchwords_in_lyric info_arr, searchword
     "show_searchwords_in_lyric"
+    # lyric_original = info_arr[0].lyric.clone
+    # pp "lyric_original = #{lyric_original}"
     reg = /(#{searchword})/
     content_tag(:ul) do
       info_arr.each do |info|
+        lyric_original = info.lyric.clone
         index_arr = []
         lyric_tmp = info.lyric
 
         while (index = lyric_tmp =~ reg) do
           pp "lyric_tmp = #{lyric_tmp}"
+          pp "index = #{index}"
           if index_arr.length == 0
             pp'IUIU'
             index_arr << index
           else
             pp'ABAB'
-            index += index_arr[-1] + searchword.length
-            index_arr << index
+            pp "index_arr[-1] = #{index_arr[-1]}"
+            insert_index = index + index_arr[-1] + searchword.length
+            index_arr << insert_index
           end
           lyric_tmp = lyric_tmp.slice!(index+searchword.length..-1)
           pp "lyric_tmp = #{lyric_tmp}"
         end
-        pp "index_arr = #{index_arr}"
+        pp "========index_arr = #{index_arr}"
+
+
+        index_arr.each do |index|
+          concat(
+            content_tag(:li) do
+              start_index = index-5
+              if start_index < 0 
+                start_index = 0
+              end
+              length = index - start_index
+              pp "lyric_original = #{lyric_original}"
+              pp "start_index = #{start_index}"
+              pp "length = #{length}"
+              concat lyric_original.slice(start_index,length)
+              concat content_tag(:b, searchword)
+              concat lyric_original.slice(index+searchword.length, 5)
+              concat "(#{info.lyric_type.name}#{info.lyric_order} by #{info.singer.name})"
+            end
+          )
+        end
 
 
         pp "index = #{index}"
 
-        matched = info.lyric.scan(reg)
-        pp "MATCHED = " + matched.to_s
-        matched.each do |mtc|
-          concat(
-            content_tag(:li) do
-              concat mtc[0]
-              concat content_tag(:b, mtc[1])
-              concat mtc[2]
-            end
-          )
-        end
       end
     end
     # <% info_arr.each do |info| %>
