eed1cdc8d3276cd4d50304eb4ce8b271673eb38b
Yuji Koyama <gladiolus_vstl@gladiolus.egoism.jp>
2018-04-15 12:29:26 +0000

    180415 ふりがな検索を簡易実装

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,21 +1,33 @@
 class SearchController < ApplicationController
   def result
-    # 結果表示に必要な情報を取得する
-    # リリース日が古い順、歌詞の順番順に表示する
-    query_results = Lyric.includes(song: :cd)
-                         .where("lyric like '%#{params[:searchword]}%'")
-                         .order(
-                           "cds.released_at ASC", 
-                           lyric_order: :ASC)
-    # 曲ごとに結果を表示するため、song_idを主キーとするハッシュにまとめる
-    # ハッシュの中身は配列になっているので表示のときには二重にeach_withしてやる必要がある
-    hash_to_hit_song_infos(query_results)
+    pp params
+    if (params[:searchtype] != 'ルビ検索')
+      # 結果表示に必要な情報を取得する
+      # リリース日が古い順、歌詞の順番順に表示する
+      query_results = Lyric.includes(song: :cd)
+                          .where("lyric like '%#{params[:searchword]}%'")
+                          .order("cds.released_at ASC", lyric_order: :ASC)
+      # 曲ごとに結果を表示するため、song_idを主キーとするハッシュにまとめる
+      # ハッシュの中身は配列になっているので表示のときには二重にeach_withしてやる必要がある
+      hash_to_hit_song_infos(:lyric, query_results)
+    else
+      # 結果表示に必要な情報を取得する
+      # リリース日が古い順、歌詞の順番順に表示する
+      query_results = Lyric.includes(song: :cd)
+                          .where("ruby like '%#{params[:searchword]}%'")
+                          .order("cds.released_at ASC", lyric_order: :ASC)
+      # 曲ごとに結果を表示するため、song_idを主キーとするハッシュにまとめる
+      # ハッシュの中身は配列になっているので表示のときには二重にeach_withしてやる必要がある
+      hash_to_hit_song_infos(:lyric, query_results)
+    end
   end
 
   private
   # 曲ごとに結果を表示するため、song_idを主キーとするハッシュにまとめる
   # ハッシュの中身は配列になっているので表示のときには二重にeach_withしてやる必要がある
-  def hash_to_hit_song_infos query_results
+  def hash_to_hit_song_infos searchtype, query_results
+    pp searchtype
+
     @hit_song_infos = {}
 
     query_results.each do |result|
--- app/helpers/search_helper.rb
+++ app/helpers/search_helper.rb
@@ -41,4 +41,47 @@ module SearchHelper
       end
     end
   end
+
+  def show_searchwords_in_ruby info_arr, searchword
+    content_tag(:ul) do
+      # info_arrはLyricモデルの配列
+      info_arr.each do |info|
+        lyric_original = info.ruby
+        index_arr = []
+
+        # 正規表現だとマッチングが重なる部分が
+        # うまくいかないのでindex()を使う
+        offset = 0
+        while (index = lyric_original.index(searchword, offset)) do
+          index_arr << index
+          offset = index + 1
+        end
+
+        # index_arrにはsearchwordの出現位置が格納されている
+        index_arr.each do |index|
+          concat(
+            content_tag(:li) do
+              # lyricの先頭付近にsearchwordがあった場合の対策
+              start_index = index - 5
+              if start_index < 0 
+                start_index = 0
+              end
+              length = index - start_index
+              # pp "lyric_original = #{lyric_original}"
+              # pp "start_index = #{start_index}"
+              # pp "length = #{length}"
+              
+              # searchword直前の５文字切り出し
+              concat lyric_original.slice(start_index,length)
+              # searchwordを切り出して強調
+              concat content_tag(:b, searchword)
+              # searchword直後の５文字切り出し
+              concat lyric_original.slice(index+searchword.length, 5)
+              concat "(#{info.lyric_type.name}#{info.part_lyric_order} by #{info.singer.name})"
+            end
+          )
+        end
+      end
+    end
+  end
 end
