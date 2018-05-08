b67c9c76fa487133c415a75d1c5511513c7ddb7f
Yuji Koyama <gladiolus_vstl@gladiolus.egoism.jp>
2018-05-08 11:51:30 +0000

    180508 トップページ言語をjaに設定

--- public/javascripts/dtcc.js
+++ public/javascripts/dtcc.js
@@ -89,11 +89,9 @@ function onPlayerReady(e) {
     ytPlayer.mute();
     ytPlayer.setPlaybackQuality('default'); // 画質(small・medium・large・hd720・hd1080・highres・default)
   } else {
-    $("#notice").html('smapho'+navigator.userAgent.toLowerCase());
     ytPlayer.mute();
-    ytPlayer.playVideo();
     ytPlayer.setPlaybackQuality('default'); // 画質(small・medium・large・hd720・hd1080・highres・default)
-
+    ytPlayer.playVideo();
   }
 }
 // 動画再生中と再生後の動作
