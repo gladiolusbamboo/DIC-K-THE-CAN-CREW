7fc024ca98d63223aa965d26fe2c05e4b042d2b5
Yuji Koyama <gladiolus_vstl@gladiolus.egoism.jp>
2018-06-09 14:18:30 +0000

    180609 VITALIZERまでデータ入力

--- db_update.cmd
+++ db_update.cmd
@@ -1,5 +1,4 @@
 pg_dump --no-acl --no-owner -h localhost -U postgres dickthecancrew_development > dicthecancrewdb.dump
 call heroku maintenance:on
 call heroku pg:reset --confirm dicthecancrew
-call heroku pg:psql DATABASE_URL < dicthecancrew.dumpcall heroku maintenance:on
-call heroku maintenance:off
+call heroku pg:psql DATABASE_URL < dicthecancrewdb.dump
