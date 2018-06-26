9b468566178b0faddbe50f723ee4833a58f13355
Yuji Koyama <gladiolus_vstl@gladiolus.egoism.jp>
2018-06-26 13:45:17 +0000

    180626 GOまでデータ入力完了

--- db_update.cmd
+++ db_update.cmd
@@ -1,4 +1,4 @@
 pg_dump --no-acl --no-owner -h localhost -U postgres dickthecancrew_development > dicthecancrewdb.dump
-call heroku maintenance:on
-call heroku pg:reset --confirm dicthecancrew
-call heroku pg:psql DATABASE_URL < dicthecancrewdb.dump
+heroku maintenance:on
+heroku pg:reset --confirm dicthecancrew
+heroku pg:psql DATABASE_URL < dicthecancrewdb.dump
