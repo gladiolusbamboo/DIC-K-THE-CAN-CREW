5d8021099530c9ee085fb5ebddf2a9f6e7ad7892
Yuji Koyama <gladiolus_vstl@gladiolus.egoism.jp>
2019-01-13 18:15:34 +0900

    190113 Heroku Seed 動作確認

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,16 +6,29 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+artist =
 Artist.create(
-    name: '三浦大知'
+    name: 'UL'
 )
 
+cd =
 Cd.create(
     name: 'ULTRAP',
     released_at: '2014-03-07'
 )
 
+lyrUrl =
 LyricUrl.create(
     url: 'https://www.utamap.com/showkasi.php?surl=k-140305-145',
     lyric_website_id: LyricWebsite.find_by(name: 'うたまっぷ').id
+)
+
+Song.create(
+    lyric_url_id: lyrUrl.id,
+    name: 'UL-TRAP',
+    lyricist: 'MCU & LITTLE',
+    composer: 'MCU, LITTLE & KREVA',
+    arranger: 'KREVA',
+    cd_id: cd.id,
+    artist_id: artist.id
 )
