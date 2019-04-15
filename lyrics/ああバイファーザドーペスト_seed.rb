56010db47e752e4810788243ba9f5cd465ea06b4
Yuji Koyama <gladiolus_vstl@gladiolus.egoism.jp>
2019-04-15 12:40:38 +0900

    190415 ああバイファー・ザ・ドーペスト　データ入力

--- lyrics/ああバイファーザドーペスト_seed.rb
+++ lyrics/ああバイファーザドーペスト_seed.rb
@@ -1,11 +1,11 @@
-artist_name =    'BY PHAR THE DOPEST'
-lyricUrl_url =   nil
-cd_name =        'BY PHAR THE DOPEST'
-cd_released_at = '1998-06-18'
-song_name =      'ああバイファー・ザ・ドーペスト'
-song_lyricist =  'UNCREDITED'
-song_composer =  'UNCREDITED'
-song_arranger =  'Rock Tee'
+artist_name =    ''
+lyricUrl_url =   ''
+cd_name =        ''
+cd_released_at = ''
+song_name =      ''
+song_lyricist =  ''
+song_composer =  ''
+song_arranger =  ''
 cds_name = []
 
 artist = Artist.find_by(name: artist_name) || Artist.create(name: artist_name)
