7ed37003ad5be8bd485a5db7c4d128c333d32a68
Yuji Koyama <gladiolus_vstl@gladiolus.egoism.jp>
2019-05-18 12:47:26 +0900

    190518 BY PHAR THE DOPEST CD-曲の関連追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,22 @@
-cd = Cd.find_by(name: 'BY PHAR THE DOPEST');
-if cd
-  cd.img_name = 'bypharthedopest'
-  cd.save
-else
-  p '見つからない -> ' + name
-end
+cd_name =        'BY PHAR THE DOPEST'
+song_name =      '切り札のカード'
+cd = Cd.find_by(name: cd_name)
+song = Song.find_by(name: song_name)
+
+CdSong.create(
+  cd_id: cd.id,
+  song_id: song.id
+)
+
+cd_name =        'BY PHAR THE DOPEST'
+song_name =      '伝導師'
+cd = Cd.find_by(name: cd_name)
+song = Song.find_by(name: song_name)
+
+CdSong.create(
+  cd_id: cd.id,
+  song_id: song.id
+)
+
+song.note = 'Al「BY PHAR THE DOPEST」には最終トラックにシークレットとして別バージョンが収録'
+song.save
